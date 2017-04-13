3d14c1a69d4a6def52e64b7b37e211c786deae71
zhangdazong <zhangdazong@jucaicat.com>
2017-04-13 11:02:55 +0800

    BaseFXViewControllerBUG

--- Classes/Base/BaseFXViewController.m
+++ Classes/Base/BaseFXViewController.m
@@ -54,6 +54,22 @@ NSString * const FXViewControllerTitleKey = @"FXViewControllerTitleKey";
     return self;
 }
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self fx_vcInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self fx_vcInit];
+    }
+    return self;
+}
+
 - (NSArray*)fx_viewModels{
     return nil;
 }
